ed7919d1249b06b0f7cff419c14876c4c222c58d
Spencer Vernon <svernon0@chicagobooth.edu>
2023-05-17 14:27:51 +0000

    Add comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_17_142420) do
+ActiveRecord::Schema.define(version: 2023_05_17_142736) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "author_id"
+    t.text "body"
+    t.integer "photo_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "photos", force: :cascade do |t|
     t.text "caption"

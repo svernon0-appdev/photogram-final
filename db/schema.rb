533fec175ab48de93bbb7f18cafe37593e8eddf9
Spencer Vernon <svernon0@chicagobooth.edu>
2023-05-17 14:06:19 +0000

    Add users

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2023_05_17_140553) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.integer "comments_count"
+    t.integer "likes_count"
+    t.boolean "private"
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
